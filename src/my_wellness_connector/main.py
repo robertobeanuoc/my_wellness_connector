6f1d6be0c82ad9d188c83894b0ba03331a652927
robertobeanuoc <rbean@uoc.edu>
2024-08-18 17:14:04 +0200

    fix: start working with database

---
+++ src/my_wellness_connector/main.py
@@ -0,0 +1,6 @@
+def sync_master_data():
+    print("Syncing master data...")
+
+
+if __name__ == "__main__":
+    print("Hello, world!")
--- src/my_wellness_connector/model.py
+++ src/my_wellness_connector/model.py
@@ -1,15 +1,36 @@
-from sqlalchemy import Column, Integer, String, DateTime
-from sqlalchemy.orm import DeclarativeBase
+from typing import List
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, event
+from sqlalchemy.orm import DeclarativeBase, Mapped, relationship, mapped_column
+import datetime
 
 
 class Base(DeclarativeBase):
-    pass
+    row_created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime, nullable=False, default=datetime.datetime.now(datetime.UTC)
+    )
+
+    row_updated_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime,
+        nullable=True,
+        default=None,
+    )
+
+
+@event.listens_for(Base, "before_insert")
+def before_insert(mapper, connection, target):
+    target.row_created_at = datetime.datetime.now(datetime.UTC)
+
+
+@event.listens_for(Base, "before_update")
+def before_update(mapper, connection, target):
+    target.row_updated_at = datetime.datetime.now(datetime.UTC)
 
 
 class ExerciseType(Base):
     __tablename__ = "exercise_type"
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(10))
+    machine_types: Mapped[List["MachineType"]] = relationship(back_populates="user")
 
 
 class SessionExercise(Base):
@@ -22,3 +43,13 @@ class SessionExercise(Base):
     fc_max: Column = Column(Integer)
     fc_avg: Column = Column(Integer)
     duration_minutes: Column = Column(Integer)
+
+
+class MachineType(Base):
+    __tablename__ = "machine_type"
+    id: Column = Column(Integer, primary_key=True, autoincrement=True)
+    name: Column = Column(String(100))
+    exercise_type_id: Column = Column(Integer, ForeignKey("exercise_type.id"))
+    exercise_type: Mapped[List["ExerciseType"]] = relationship(
+        back_populates="machine_types"
+    )
